6909a44af1d789f8915a746538b271e3e8c3f9e8
Amanda Carmo <amanda.rosa.ismart@gmail.com>
2021-05-17 19:48:23 -0300

    mapa

--- src/components/MapPage/index.js
+++ src/components/MapPage/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AppBar from "../AppBar";
+import GeoChart from "../GeoChart";
 import "./index.css";
 import { VectorMap } from "react-jvectormap"
 
@@ -7,9 +8,7 @@ import { VectorMap } from "react-jvectormap"
 export default function MapPage() {
   return (
         <div>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jvectormap/2.0.4/jquery-jvectormap.css" type="text/css" media="screen"/>
-            <p>oioioi</p>
-            
+          <GeoChart />                          
             
         </div>
 
